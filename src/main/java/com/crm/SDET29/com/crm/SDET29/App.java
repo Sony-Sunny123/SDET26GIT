bd974b18ec5f045ed031e7eb390e121b839fc765
sonys <sonys@DESKTOP-LC8G8L6>
2021-12-09 14:11:18 +0530

    test git

---
+++ src/main/java/com/crm/SDET29/com/crm/SDET29/App.java
@@ -0,0 +1,13 @@
+package com.crm.SDET29.com.crm.SDET29;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/practice/GitPractice.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.junit.Test;
+
+public class GitPractice {
+	public static void main(String args[])
+	{
+		System.out.println("sussess");
+	}
+
+}

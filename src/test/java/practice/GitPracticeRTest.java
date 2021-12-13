3b064797504948ad7a77c4054bb057f2958bdb0c
sonys <sonys@DESKTOP-LC8G8L6>
2021-12-13 13:04:39 +0530

    Sdet29 Latest

---
+++ src/test/java/practice/GitPracticeRTest.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class GitPracticeRTest {
+	
+	@Test
+	public void gitPracticeRTest()
+	{
+		System.out.println("success");
+	}
+		
+	
+
+}
